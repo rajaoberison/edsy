8df5407380ffae824c9a49b253a40c8a66ebf55c
Andry Rajaoberison <41961364+rajaoberison@users.noreply.github.com>
2019-11-19 20:56:11 -0500

    Update edsy.js

--- docs/_static/js/edsy.js
+++ docs/_static/js/edsy.js
@@ -1,1 +1,1 @@
-document.getElementsByClassName("wy-breadcrumbs-aside")[0].innerHTML = '<li class="wy-breadcrumbs-aside"><a href="https://github.com/rajaoberison/edsy/blob/master/docs/index.rst" class="fa fa-github" target="_blank"> Edit on GitHub</a></li>'
+
