f40856175e33364d66083bfe7b5126cc77020e7e
AR (Mac) <41961364+rajaoberison@users.noreply.github.com>
2020-01-16 17:49:29 -0500

    updated contents

--- build_site.R
+++ build_site.R
@@ -1,3 +1,5 @@
 setwd("/Users/rajaoberison/OneDrive/GitHub.com/rajaoberison/edsy")
 
-rmarkdown::render_site()
+rmarkdown::render_site("index.Rmd")
+rmarkdown::render_site("reading-data.Rmd")
+rmarkdown::render_site("wrangling-data.Rmd")
