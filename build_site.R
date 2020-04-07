3540eae76f691dfaecb670bc7abc4c83f5b44604
AR (Mac) <41961364+rajaoberison@users.noreply.github.com>
2020-04-07 15:01:39 -0400

    moved to bookdown

--- build_site.R
+++
@@ -1,5 +0,0 @@
-setwd("/Users/rajaoberison/OneDrive/GitHub.com/rajaoberison/edsy")
-
-rmarkdown::render_site("index.Rmd")
-rmarkdown::render_site("reading-data.Rmd")
-rmarkdown::render_site("wrangling-data.Rmd")
